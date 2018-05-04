1db3a91ef8d3b7f6ccb27efe730883f8abeffca3
DESKTOP-4KTTN5C\math1 <ito@star.rcast.u-tokyo.ac.jp>
2018-05-04 12:27:12 +0900

    Add ReplaceObject.cs and RemovePillar.cs

---
+++ Giant/Assets/Scripts/RemovePillar.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemovePillar : MonoBehaviour {
+    public bool isRemoved;
+
+	// Use this for initialization
+	void Start () {
+        isRemoved = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isRemoved)
+        {
+            isRemoved = true;
+            Selected();
+        }
+	}
+
+    public void Selected()
+    {
+        StartCoroutine("Remove");
+    }
+
+    IEnumerator Remove()
+    {
+        float scale = this.transform.localScale.y;
+        float count = scale;
+        while(count > 0.0f)
+        {
+            Vector3 pos = this.transform.position;
+            pos.y -= 0.01f;
+            this.transform.position = pos;
+            count -= 0.01f;
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+}
---
+++ Giant/Assets/Scripts/ReplaceObject.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using HoloToolkit.Unity.InputModule;
+using HoloToolkit.Unity.SpatialMapping;
+using UnityEngine;
+
+public class ReplaceObject : MonoBehaviour, IInputClickHandler{
+    public SpatialMappingManager spatialMappingManager;
+    bool placing = false;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // If the user is in placing mode,
+        // update the placement to match the user's gaze.
+
+        if (placing)
+        {
+            // Do a raycast into the world that will only hit the Spatial Mapping mesh.
+            var headPosition = Camera.main.transform.position;
+            var gazeDirection = Camera.main.transform.forward;
+
+            RaycastHit hitInfo;
+            if (Physics.Raycast(headPosition, gazeDirection, out hitInfo,
+                30.0f, SpatialMappingManager.Instance.LayerMask))
+            {
+                // Move this object's parent object to
+                // where the raycast hit the Spatial Mapping mesh.
+                this.transform.parent.position = hitInfo.point;
+
+                // Rotate this object's parent object to face the user.
+                Quaternion toQuat = Camera.main.transform.localRotation;
+                toQuat.x = 0;
+                toQuat.z = 0;
+                this.transform.parent.rotation = toQuat;
+            }
+        }
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData) {
+        // On each Select gesture, toggle whether the user is in placing mode.
+        placing = !placing;
+
+        // If the user is in placing mode, display the spatial mapping mesh.
+        if (placing)
+        {
+            SpatialMappingManager.Instance.DrawVisualMeshes = true;
+        }
+        // If the user is not in placing mode, hide the spatial mapping mesh.
+        else
+        {
+            SpatialMappingManager.Instance.DrawVisualMeshes = false;
+        }
+    }
+}
