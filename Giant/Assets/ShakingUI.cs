5a151224f6a93be7084b3d88a221817308db633e
Daichi Ito <math144377@gmail.com>
2018-05-10 19:12:13 +0900

    Add ShakingUI.cs

---
+++ Giant/Assets/ShakingUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShakingUI : MonoBehaviour {
+    private float time;
+
+    public float ampDist;
+    public float ampTime;
+	// Use this for initialization
+	void Start () {
+        time = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+        
+        Vector3 pos = this.transform.localPosition;
+        pos.y += ampDist * Mathf.Sin(ampTime * time);
+        this.transform.localPosition = pos;
+	}
+}
